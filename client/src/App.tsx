79a7d5eb0b898ef8db5cfa9a82d8a731f5d5272f
lil-aditya <adg.aids.iitj@gmail.com>
2026-05-20 01:20:29 +0530

    feat(client): add App component

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
+      <h1>CloudNative</h1>
+      <p>Client scaffolding is ready.</p>
+    </div>
+  )
+}
