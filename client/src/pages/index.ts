76d691eeb257600c3dc15b54ab6ab58a737ea186
lil-aditya <adg.aids.iitj@gmail.com>
2026-05-20 01:22:44 +0530

    chore(client): export pages

---
+++ client/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as LandingPage } from './LandingPage'
+export { default as LoginPage } from './LoginPage'
+export { default as RegisterPage } from './RegisterPage'
+export { default as DashboardPage } from './DashboardPage'
+export { default as ProfilePage } from './ProfilePage'
